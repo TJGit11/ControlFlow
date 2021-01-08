c82d7f5f5343e2d99a55c832bcadccfe8487f794
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2021-01-08 10:56:09 -0600

    practice

---
+++ src/com/tts/Main.java
@@ -0,0 +1,122 @@
+package com.tts;
+
+public class Main {
+
+    private static String method2(){
+        return "five";
+    }
+
+    private static String method1(){
+        return "three";
+    }
+
+    public static void main(String[] args) {
+//        // use of If-Else statements
+//        int a = 6;
+//        if(a==1) {
+//            System.out.println("A is 1");
+//        }
+//            else if(a==2){
+//                System.out.println("A is 2");
+//            }
+//            else if(a==3) {
+//            System.out.println("A is 3");
+//        }
+//            else if(a==4) {
+//            System.out.println("A is 4");
+//        }
+//
+//        }
+//
+//
+//    }
+//    }
+//
+//        int a = 9;
+//        switch (a) {
+//            case 0:
+//                System.out.println("zero");
+//                break;
+//            case 1:
+//                int b = (a + 5);
+//                System.out.println("B is:" + b);
+//                break;
+//            case 2:
+//                int c = (a * 5);
+//                System.out.println("C is: " + c);
+//                break;
+//            case 3:
+//                int d = (a + 50);
+//                System.out.println(d);
+//                break;
+//            case 4:
+//                System.out.println("We have four");
+//                break;
+//            default:
+//                System.out.println("Nothing to see here");
+//                break;
+//
+//        }
+
+//        //Use of For Loop
+//        String[] names = new String[]{"Randy", "Aaliyah", "Winter", "Joi", "Ryan", "Megan", "SeDerek"};
+//        for (int i = 0; i < names.length; i++) {
+//            System.out.println("The current index is: " + i + " " + names[i]);
+//            System.out.println("The current name is " + names[i]);
+//        }
+
+
+//        // Use of Enhanced For Loop
+//        String[] names = new String[]{"Randy", "Aaliyah", "Winter", "Joi",
+//                "Ryan", "Megan", "SeDerek"};
+//        for (String s : names) {
+//
+//            System.out.println("Name is: " + s);
+//        }
+
+        //Use of Nested For Loop
+
+//        String[] names = new String[]{"Randy", "Aaliyah", "Winter", "Joi", "Ryan", "Megan", "SeDerek"};
+//        for (int i = 0; i < names.length; i++) {
+//            System.out.println("The current index is: " + i + " " + names[i]);
+//            System.out.println("The current name is " + names[i]);
+//        }
+//        for (int j=0; j<20; j++){
+//            System.out.print(j);
+//            for(int k=0; k<20; k++){
+//                System.out.print("*");
+//                if (k == 19){
+//                    System.out.println();
+//                }
+//            }
+//        }
+        /*
+        // Use of While Loop
+        boolean flag = false;
+        int x= 0;
+
+        do{
+            x++;
+            System.out.println("Hello" + x );
+            if (x == 55555){
+                flag = false;
+            }
+        }
+        while (flag); {
+            System.out.println("Stop Here");
+        }
+
+         */
+
+        //Use of Ternary Operation
+        boolean flag = false;
+
+        int z = (flag ? 3 : 5);
+
+        System.out.println("Z is : " + z);
+
+        String m = (flag ? method1() : method2() );
+
+        System.out.println("M is: " + m);
+    }
+}
